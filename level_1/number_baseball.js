a5883910fcb27568a192739cca0f838b9319d494
Tynahan <nusilite@gmail.com>
2025-11-20 13:53:32 +0900

    chore(1_baseball): write down what to do

--- level_1/number_baseball.js
+++ level_1/number_baseball.js
@@ -22,6 +22,16 @@ const submit = (candidate) => {
     return `${perferctCount}S ${numberOnlyCorrectCount}B`
 }
 
-const run = () => submit(2134)
+// const run = () => submit(2134)
+// console.log(run())
 
-console.log(run())
+const digitArray = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+
+const infoArray = [null, null, null, null, null] // 5th: not using number
+const canditateArray = []
+
+// 앞으로 할 것
+// 9부터 차례대로 꽉 채우면서
+// 없는 것 찾고 볼 찾으면
+// 없 -> 볼 -> 스트라이크 찾음
+// 볼 -> 없 -> 스트라이크 찾음
